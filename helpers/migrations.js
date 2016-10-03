377c66e5f052223c22593afa40279b815b82b5af
Trey White <trey@2codemonkeys.com>
2016-10-02 22:55:12 -0400

    Seeding data for playlist-songs

--- helpers/migrations.js
+++ helpers/migrations.js
@@ -1,6 +1,17 @@
 function MigrationHelper() {}
 
 /**
+ * This function seeds data provided for the supplied arrayOfInserts
+ */
+function seed(db, tableName, columns, arrayOfInserts) {
+  var counter = 0;
+  while(counter < arrayOfInserts.length) {
+    db.insert(tableName, columns, arrayOfInserts[counter], function() {})
+    counter++;
+  }
+}
+
+/**
  * This function seeds data provided and adds on the needed timestamps to the
  * supplied arrayOfInserts
  */
@@ -9,13 +20,15 @@ function seedWithTimestamps(db, tableName, columns, arrayOfInserts) {
   while(counter < arrayOfInserts.length) {
     arrayOfInserts[counter].push("now()"); // Adding createdAt timestamp
     arrayOfInserts[counter].push("now()"); // Adding updatedAt timestamp
-    db.insert(tableName, columns, arrayOfInserts[counter], function() {})
     counter++;
   }
+
+  seed(db, tableName, columns, arrayOfInserts);
 }
 
 /**
- *
+ * This function creates an array with ID indexes for the table and attribute
+ * (value) as supplied.  This function is setup to be used with promises.
  */
 function getAttributeIdMap(db, tableName, value) {
   return {
@@ -32,6 +45,7 @@ function getAttributeIdMap(db, tableName, value) {
 }
 
 MigrationHelper.prototype = {
+  seed: seed,
   seedWithTimestamps: seedWithTimestamps,
   getAttributeIdMap: getAttributeIdMap
 }
