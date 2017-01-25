69c5b32ccd3345d09c7cf3dbc2ee8c75e49c4616
Jeremy Altman <jeremy@540.co>
2017-01-25 09:13:29 -0500

    Reverting deploy script

--- .codedeploy/scripts/start.sh
+++ .codedeploy/scripts/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # migrate the database
-docker run --rm --env-file /home/ec2-user/.env.jukebox DOCKER_TAG rails db:reset
+docker run --rm --env-file /home/ec2-user/.env.jukebox DOCKER_TAG rails db:migrate
+docker run --rm --env-file /home/ec2-user/.env.jukebox DOCKER_TAG rails db:seed
 
 # start the container
 docker run -d --name jukebox-api --env-file /home/ec2-user/.env.jukebox -p 3000:3000 DOCKER_TAG bundle exec rails s -b 0.0.0.0
